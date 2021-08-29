3fe54120bb1329a6a1869f02f273e991552c6a85
orion <inoukakis@gmail.com>
2021-08-29 18:48:08 +0200

    ex 3.4

---
+++ src/main/scala/chapter3/ex3_4/Main.scala
@@ -0,0 +1,85 @@
+package me.ioannis.noukakis
+package chapter3.ex3_4
+
+import scala.annotation.tailrec
+
+sealed trait List[+A] // `List` data type, parameterized on a type, `A`
+
+case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+
+/* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
+which may be `Nil` or another `Cons`.
+ */
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List { // `List` companion object. Contains functions for creating and working with lists.
+  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+    case Nil => 0 // The sum of the empty list is 0.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, tail) => tail
+  }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(_, tail) => Cons(h, tail)
+  }
+
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Nil => Nil
+    case l if n <= 0 => l
+    case Cons(_, tail) => drop(tail, n - 1)
+  }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+
+  def init[A](l: List[A]): List[A] = ???
+
+  def length[A](l: List[A]): Int = ???
+
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
+
+  def map[A, B](l: List[A])(f: A => B): List[B] = ???
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(List.drop(List("a", "b", "c", "d"), 1))
+    println(List.drop(List("a", "b", "c", "d"), 2))
+    println(List.drop(List("a", "b", "c", "d"), 3))
+    println(List.drop(List("a", "b", "c", "d"), 100))
+  }
+}
